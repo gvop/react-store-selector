0fe40f7754c2637df363d6f790494cb2ce9a2d1b
Guus van Ooijen <guusvanooijen@gmail.com>
2018-01-13 19:42:24 +0000

    Initial commit

---
+++ __tests__/index.test.js
@@ -0,0 +1,19 @@
+import { createStore, returnStore } from '../index';
+
+describe('Store Selector HOC', () => {
+  it('Should return an empty store at the before createStore is being called', () => {
+    const initialStore = returnStore();
+    expect(initialStore).toEqual({});
+  });
+
+  it('Should set the initial state with the object createStore is being called with', () => {
+    const data = {
+      test: 'test data',
+      data: {},
+    };
+    createStore(data);
+    const initialStore = returnStore();
+    expect(initialStore).toEqual(data);
+  });
+});
+
